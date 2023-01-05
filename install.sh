ce4c3d4f1ace8d2bac8dc21409485e0b49deec17
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-01-05 21:30:16 +0530

    Bloat removal

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo "Installing Linux (Debian amd64)...."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update -y > /dev/null 2>&1
-sudo apt install apt-transport-https ufw fish apache2 php sublime-text xfce4 xarchiver wine firefox-esr mesa-utils git xfce4-goodies pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus neofetch perl p7zip unzip zip curl tar git python3 python3-pip net-tools openssl tigervnc-standalone-server tigervnc-xorg-extension -y
+sudo apt install apt-transport-https ufw fish apache2 php xfce4 xarchiver firefox-esr mesa-utils git  pv nmap nano apt-utils dialog terminator autocutsel dbus-x11 dbus  perl p7zip unzip zip curl tar git python3 python3-pip net-tools openssl tigervnc-standalone-server tigervnc-xorg-extension -y
 export HOME="$(pwd)"
 export DISPLAY=":0"
 cd $HOME 2> /dev/null
